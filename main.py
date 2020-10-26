af6830453499d271e8da6a597e35da9d82cd7927
dlapochkin <73088970+dlapochkin@users.noreply.github.com>
2020-10-26 10:30:36 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-years = int(input())
+# Project #1
+# Developer:   Lapochkin D.
+
+fwv = 35000000                                # volume of fresh water in the world in cubic kilometers
+glv = fwv * 0.21                              # volume of the Great Lakes in cubic kilometers
+area = 7664000                                # area of states in square kilometers
+wl = (glv / area) * 1000                      # water layer thickness in meters
+print(round(wl), 'метров')
